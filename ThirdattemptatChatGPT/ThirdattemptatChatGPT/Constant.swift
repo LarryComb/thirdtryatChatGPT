c6c72ca4f24fe8d874029155fcb9dfe6138307f1
larry <larrycomb@gmail.com>
2023-01-23 20:40:43 -0800

    update

---
+++ ThirdattemptatChatGPT/ThirdattemptatChatGPT/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  ThirdattemptatChatGPT
+//
+//  Created by LARRY COMBS on 1/23/23.
+//
+
+import Foundation
+
+enum Constants {
+    static let openAiAPIKey = ""
+}
--- ThirdattemptatChatGPT/ThirdattemptatChatGPT/ContentView.swift
+++ ThirdattemptatChatGPT/ThirdattemptatChatGPT/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var chatMessages: [ChatMessage] = ChatMessage.sampleMessages
+    @State var messageText: String = ""
+    
     var body: some View {
-        Lazy VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello")
+        VStack{
+            ScrollView{
+                LazyVStack {
+                    ForEach(chatMessages, id: \.id) { message in
+                        messageView(message: message)
+                    }
+                }
+            }
+            HStack {
+                TextField("Enter A Message", text: $messageText){
+                    
+                }
+                    .foregroundColor(.blue)
+                    .padding()
+                    .background(.gray.opacity(0.1))
+                    .cornerRadius(12)
+                Button {
+                    sendMessage()
+                } label: {
+                    Text("Send")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.black)
+                        .cornerRadius(12)
+                    
+                }
+            }
         }
         .padding()
+    }
+    
+    func messageView(message: ChatMessage) -> some View {
+        HStack{
+            if message.sender == .me { Spacer() }
+            Text(message.content)
+                .foregroundColor(message.sender == .me ? .white : .black).padding()
+                .background(message.sender == .me ? .blue : .gray.opacity(0.1)).cornerRadius(16)
+            if message.sender == .GPT { Spacer() }
+        }
+    }
+    
+    func sendMessage() {
+        messageText = ""
+        print(messageText)
     }
 }
 
@@ -23,4 +63,26 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ChatMessage{
+    let id: String
+    let content: String
+    let dateCreated: Date
+    let sender: MessageSender
+}
+
+enum MessageSender {
+    case me
+    case GPT
+}
+
+extension ChatMessage {
+    static let sampleMessages = [
+        ChatMessage(id: UUID().uuidString, content: "SampleMessage From me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "SampleMessage From GPT", dateCreated: Date(), sender: .GPT),
+        ChatMessage(id: UUID().uuidString, content: "SampleMessage From me", dateCreated: Date(), sender: .me),
+        ChatMessage(id: UUID().uuidString, content: "SampleMessage From GPT", dateCreated: Date(), sender: .GPT),
+    ]
+    
 }
