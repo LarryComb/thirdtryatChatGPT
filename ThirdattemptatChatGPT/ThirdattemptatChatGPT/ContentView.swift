d6090df6669fbc317b64456d13a7f82b02e6eabe
larry <larrycomb@gmail.com>
2023-01-23 19:33:46 -0800

    adding xcode

---
+++ ThirdattemptatChatGPT/ThirdattemptatChatGPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThirdattemptatChatGPT
+//
+//  Created by LARRY COMBS on 1/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Lazy VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThirdattemptatChatGPT/ThirdattemptatChatGPT/ThirdattemptatChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThirdattemptatChatGPTApp.swift
+//  ThirdattemptatChatGPT
+//
+//  Created by LARRY COMBS on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct ThirdattemptatChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
